22d4a144b745c4bb8b530e190d0455e7b99adb77
FarhanKaleem3001 <farhan@techoriginator.com>
2026-01-01 18:33:29 +0530

    add file

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:babysoft/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'provider/villa_provider.dart';
+
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => VillaProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
